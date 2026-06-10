9d8e24eda4006a34bdece86cd01d084a632fe2df
beastful <89183856+beastful@users.noreply.github.com>
2026-06-10 20:40:30 +0300

    Server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+import { Server } from '@hocuspocus/server'
+import { Logger } from '@hocuspocus/extension-logger'
+import { SQLite } from '@hocuspocus/extension-sqlite'
+
+const server = new Server({
+  name: 'hocuspocus',
+  port: 1234,
+  timeout: 60000,
+  debounce: 5000,
+  maxDebounce: 30000,
+  quiet: true,
+  websocketOptions: { maxPayload: 1024 * 1024 },
+  extensions: [
+    new Logger(),
+    new SQLite({ database: 'collab.db' }),
+  ]
+})
+
+server.listen()
+console.log('Hocuspocus server running on ws://localhost:1234')
